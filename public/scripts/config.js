c8baece86dcd7f166e3419fb99ea2283125cc6df
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-08-27 19:58:57 +0300

    loginbar html partial added, # href in home.html added, system.js added

---
+++ public/scripts/config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Solara on 26/08/2015.
+ */
+System.config({
+    transpiler: 'babel',
+    map: {
+        "babel": '../node_modules/babel-core/browser.js'
+    }
+});
---
+++ public/scripts/loginController.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Solara on 26/08/2015.
+ */
+//TODO: config System.js
+function getLoginData(){
+    console.log('Heyyy');
+var $loginForm = $('#loginForm');
+    $('#loginSubmit').on('click', function (){
+        currentUser.username = $('#username').val();
+        currentUser.password = $('#pass').val();
+        // post data for the user on server, check in db for the user
+        console.log(currentUser);
+        return false;
+    });
+}
+
+export { getLoginData }
