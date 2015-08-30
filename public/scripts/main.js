601dad7e3fdd40e0c2553e635e1a48d913fae862
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-08-30 20:48:10 +0300

    changes on routing. some bugs fixed, some new in... C'est la vie

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,12 +3,15 @@
 //import '../lib/jquery/dist/jquery.js';
 import '../lib/jquery-2.0.2.js';
 import lc from './loginController.js';
-//import toastr from '../lib/toastr/toastr.js';
+import toastr from '../lib/toastr/toastr.js';
 
 var start = $(function () {
     var $mainContainer = $('#main-container'),
         $groupContainer = $('#group-container'),
         $loginBar = $('#loginBar');
+
+    document.location.hash = "#/";
+    document.location.hash = "#home";
 
     /* Some usefull options for toastr:
      toastr.options = {
@@ -28,38 +31,38 @@ var start = $(function () {
      "showMethod": "fadeIn",
      "hideMethod": "fadeOut"
      };*/
-	 
+
     //TODO: make div for this html which should change content on login
     $loginBar.load('partials/loginBar.html', function () {
-		lc.getLoginData();
+        lc.getLoginData();
         /*$('#loginSubmit').on('click', function () {
-            currentUser.username = $('#username').val();
-            currentUser.password = $('#pass').val();
+         currentUser.username = $('#username').val();
+         currentUser.password = $('#pass').val();
 
-            // DONE: post data for the user on server, check in db for the user
-            // TODO: extract in module, set promises
+         // DONE: post data for the user on server, check in db for the user
+         // TODO: extract in module, set promises
 
-            $.ajax({
-                url: '/User',
-                contentType: 'application/json',
-                data: currentUser,
-                success: function (data) {
-                    console.log(data);
-                    // TODO: this should be plugged in the 'success' function in the POST ajax request
-                    $('#loginForm').hide();
-                    // TODO: think of handlebars
-                    $('#user-profile-dropdown').text(data.username);
-                    $('#logged-user').show();
-                },
-                error: function (data) {
-					console.log('wrong user');
-                    //toastr.options = {"positionClass": "toast-top-right"};
-                    //toastr.error('Username or password are not valid!');
-                }
-            });
+         $.ajax({
+         url: '/User',
+         contentType: 'application/json',
+         data: currentUser,
+         success: function (data) {
+         console.log(data);
+         // TODO: this should be plugged in the 'success' function in the POST ajax request
+         $('#loginForm').hide();
+         // TODO: think of handlebars
+         $('#user-profile-dropdown').text(data.username);
+         $('#logged-user').show();
+         },
+         error: function (data) {
+         console.log('wrong user');
+         //toastr.options = {"positionClass": "toast-top-right"};
+         //toastr.error('Username or password are not valid!');
+         }
+         });
 
-            return false;
-        });*/
+         return false;
+         });*/
 
         /*  $('#sign-up-button').on('click', function () {
          $mainContainer.load('partials/sign-up-form.html');
@@ -93,50 +96,114 @@ var start = $(function () {
                 return {container: $mainContainer, controller: homeController};
             case 'sign-up-form':
                 return {container: $mainContainer, controller: signupController};
+            case 'sign-up-group':
+                return {container: $mainContainer, controller: signupGroupController};
             case 'group-nav':
                 return {container: $mainContainer, controller: groupNavController};
             case 'feed':
                 return {container: $groupContainer, controller: feedController};
             case 'meetings':
-                return {container: $groupContainer, controller:meetingsController};
+                return {container: $groupContainer, controller: meetingsController};
             case 'todo-list':
-                return {container: $groupContainer, controller:todoController};
+                return {container: $groupContainer, controller: todoController};
             case 'chat':
-                return {container: $groupContainer, controller:chatController};
+                return {container: $groupContainer, controller: chatController};
             case 'calendar':
-                return {container: $groupContainer, controller:calendarController};
+                return {container: $groupContainer, controller: calendarController};
 
+            //DEBUG:
+            case 'test':
+                return {container: $mainContainer, controller: testController};
 
             default:
-                return false;
+                return{container: $mainContainer, controller: homeController};
                 break;
+        }
 
-
-    };
     };
 
     // CONTROLLERS
-    var homeController = function () {
-        console.log('home page loaded');
-    };
-    var feedController = function () {
-        console.log('feed page loaded');
-    };
-    var meetingsController = function () {
-        console.log('meetings page loaded');
-    };
-    var todoController = function () {
-        console.log('todo page loaded');
-    };
-    var calendarController = function () {
-        console.log('calendar page loaded');
+    var testController = function () {
+        //CREATE GROUP
+        var btnSumbit = $('#group-signup-submit');
+        var newGroup;
+        btnSumbit.on('click', function () {
+            console.log('CLICK');
+            newGroup = {
+                name: $('#input-group-name').val(),
+                password: $('#input-group-password').val(),
+                description: $('#input-group-desc').val(),
+                git: $('#input-git-link').val()
+            };
+
+            $.ajax({
+                method: "POST",
+                url: '/groups/signup',
+                contentType: 'application/json',
+                data: JSON.stringify(newGroup),
+                success: function (data) {
+                    console.log(data);
+                },
+                error: function (data) {
+                    console.log(data);
+                }
+            });
+        });
+        // ADD MEMBER TO PREV REGISTRED GROUP
+        var btnAdd = $('#add-member-btn');
+        btnAdd.on('click', function () {
+            var member = $('#input-add-member').val();
+
+
+            $.ajax({
+                method: 'POST',
+                url: '/groups/add/member',
+                contentType: 'application/json',
+                data: JSON.stringify({name: newGroup.name, username: member}),
+                success: function (data) {
+                    console.log('MEMBER ADDED');
+                    console.log(data);
+                },
+                error: function (data) {
+                    console.log(data);
+                }
+            });
+
+        });//end btn onclick
+
+
     };
 
-    var groupNavController = function () {
-        console.log('group navigation loaded');
+    var signupGroupController = function () {
+        $groupContainer.html('');
+        var btnSumbit = $('#group-signup-submit');
+        var newGroup;
+        btnSumbit.on('click', function () {
+            console.log('CLICK');
+            newGroup = {
+                name: $('#input-group-name').val(),
+                password: $('#input-group-password').val(),
+                description: $('#input-group-desc').val(),
+                git: $('#input-git-link').val()
+            };
+
+            $.ajax({
+                method: "POST",
+                url: '/groups/signup',
+                contentType: 'application/json',
+                data: JSON.stringify(newGroup),
+                success: function (data) {
+                    console.log(data);
+                },
+                error: function (data) {
+                    console.log(data);
+                }
+            });
+        });
     };
 
     var signupController = function () {
+        $groupContainer.html('');
         console.log('Hello from SIGN UP controller');
         var $signUpSubmit = $('#signUpSubmit');
 
@@ -150,7 +217,7 @@ var start = $(function () {
                 email: $email,
                 password: $password
             };
-console.log(newUser);
+            console.log(newUser);
             $.ajax({
                 method: 'POST',
                 url: '/signup',
@@ -185,8 +252,31 @@ console.log(newUser);
 
     };
 
+    var homeController = function () {
+        $groupContainer.html('');
+    };
 
-// Controller for Chat, must be moved to another js module
+    var feedController = function () {
+        console.log('feed page loaded');
+    };
+
+    var meetingsController = function () {
+        console.log('meetings page loaded');
+    };
+
+    var todoController = function () {
+        console.log('todo page loaded');
+    };
+
+    var calendarController = function () {
+        console.log('calendar page loaded');
+    };
+
+    var groupNavController = function () {
+        $groupContainer.html('');
+        console.log('group navigation loaded');
+    };
+
     var chatController = function () {
         var socket = io(),
             $messageField = $("#message"),
