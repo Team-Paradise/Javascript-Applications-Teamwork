1438d347632a345b2458393a768385f97d7a497e
baretata <zlatkoatanasov92@gmail.com>
2015-09-01 11:38:27 +0300

    Home page reworked with css properly and button Join Now does not work if user is already logged in.

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -1,5 +1,20 @@
+import toastr from 'lib/toastr/toastr.js';
+
 export default function homeController() {
-    var $groupContainer = $('#group-container');
+
+    var $groupContainer = $('#group-container'),
+	$mainContainer = $('#main-container'),
+    $jumbotronButton = $('#jumbotron-join-button');
+	
     $groupContainer.html('');
+	
+	if (!!localStorage.getItem('user')) {
+        $jumbotronButton.on('click', function(){
+            toastr.options = {"positionClass": "toast-top-center"};
+            toastr.error('You are already registered.');
+            $jumbotronButton.removeAttr('href');
+        });
+	}
+	
     console.log('home page loaded');
 }
