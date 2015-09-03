3a90dc706722f9b5b5a9563a4a4afd81a2ae0fa1
baretata <zlatkoatanasov92@gmail.com>
2015-09-04 02:12:20 +0300

    Calendar now visualize the meetings that has been arranged in the currently selected group

--- public/scripts/controllers/calendarController.js
+++ public/scripts/controllers/calendarController.js
@@ -4,37 +4,34 @@ import 'lib/kendo.all.min.js';
 export default function calendarController() {
     $(function(){
         $(document).ready(function () {
-            var startTime = '3/9/2015 08:00';
-            var title = "Meeting";
-            var meeting = {
-                date: kendo.parseDate(startTime, "dd/MM/yyyy HH:mm"),
-                about: title
-            };
+            var meetingsFromDb = [];
             
-            var endTime = new Date(meeting.date.getTime());
-            var endMeetingTime = new Date(endTime.setTime(endTime.getTime() + 120*60*1000));
-            
-            //get data from the request and use it in the calendar
-            var meetingsFromDb = {};
-            
-            /*$.get('/groups/meetings', function(data) {
-                console.log(data);
-            });*/
-            
-            console.log('----');
-            console.log(meetingsFromDb);
-            
-            $("#scheduler").kendoScheduler({
-                allDaySlot: false,
-                height: 600,
-                editable: false,
-                isAllDay: true,
-                format: "dd/MM/yyyy HH:mm",
-                dataSource: [{
-                  start: meeting.date,
-                  end: endMeetingTime,
-                  title: meeting.about
-                }]
+            $.ajax({
+                url: '/groups/meetings',
+                contentType: 'application/json',
+                data: {name: JSON.parse(localStorage.getItem('current-group'))},
+                success: function (data) {
+                    data.meetings.map(function(item, index) {
+                        item.date = kendo.parseDate(item.date, "dd/MM/yyyy HH:mm");
+                        var endTime = new Date(item.date.getTime());
+                        var endMeetingTime = new Date(endTime.setTime(endTime.getTime() + 120*60*1000));
+                        
+                        meetingsFromDb[index] = {
+                            start: item.date,
+                            end: endMeetingTime,
+                            title: item.about
+                        };
+                    });
+                    
+                    $("#scheduler").kendoScheduler({
+                        allDaySlot: false,
+                        height: 600,
+                        editable: false,
+                        isAllDay: true,
+                        format: "dd/MM/yyyy HH:mm",
+                        dataSource: meetingsFromDb
+                    });
+                }
             });
         });
     });
