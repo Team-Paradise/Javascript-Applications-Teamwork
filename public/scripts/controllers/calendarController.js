99038264e790177cb910b5ec0d734a2e65739797
baretata <zlatkoatanasov92@gmail.com>
2015-09-04 00:43:00 +0300

    Group partials styles fixed and calendar functionality added

--- public/scripts/controllers/calendarController.js
+++ public/scripts/controllers/calendarController.js
@@ -1,9 +1,41 @@
+import 'lib/jquery/dist/jquery.js';
 import 'lib/kendo.all.min.js';
 
 export default function calendarController() {
     $(function(){
-        $("#scheduler").kendoScheduler({
-            height: 600,
+        $(document).ready(function () {
+            var startTime = '3/9/2015 08:00';
+            var title = "Meeting";
+            var meeting = {
+                date: kendo.parseDate(startTime, "dd/MM/yyyy HH:mm"),
+                about: title
+            };
+            
+            var endTime = new Date(meeting.date.getTime());
+            var endMeetingTime = new Date(endTime.setTime(endTime.getTime() + 120*60*1000));
+            
+            //get data from the request and use it in the calendar
+            var meetingsFromDb = {};
+            
+            /*$.get('/groups/meetings', function(data) {
+                console.log(data);
+            });*/
+            
+            console.log('----');
+            console.log(meetingsFromDb);
+            
+            $("#scheduler").kendoScheduler({
+                allDaySlot: false,
+                height: 600,
+                editable: false,
+                isAllDay: true,
+                format: "dd/MM/yyyy HH:mm",
+                dataSource: [{
+                  start: meeting.date,
+                  end: endMeetingTime,
+                  title: meeting.about
+                }]
+            });
         });
     });
 }
